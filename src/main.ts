f21f4f6c341beec469c38e87c558f8f9e89eed8a
Manases Lovera <manaseslovera07@gmail.com>
2024-10-09 19:22:39 -0400

    global middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { logger } from './logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Global middleware
+  app.use(logger);
   await app.listen(3000, () => {
     console.log(`listening on port 3000`);
   });
