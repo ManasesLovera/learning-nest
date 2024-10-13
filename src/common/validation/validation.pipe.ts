f9d22a7acf495b924cb9ce0c6222cd09fba1079f
Manases Lovera <manaseslovera07@gmail.com>
2024-10-12 23:57:47 -0400

    using zod

--- src/common/validation/validation.pipe.ts
+++ src/common/validation/validation.pipe.ts
@@ -1,4 +1,5 @@
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { z, ZodSchema } from 'zod';
 
 @Injectable()
 export class ValidatonPipe implements PipeTransform {
@@ -6,3 +7,28 @@ export class ValidatonPipe implements PipeTransform {
         return value;
     }
 }
+
+export class ZodValidationPipe implements PipeTransform {
+
+    constructor(private schema: ZodSchema) {}
+
+    transform(value: unknown, metadata: ArgumentMetadata) {
+        try {
+            const parsedValue = this.schema.parse(value);
+            return parsedValue;
+          } catch (error) {
+            throw new BadRequestException('Validation failed');
+          }
+    }
+
+}
+
+export const createCatSchema = z
+    .object({
+        name: z.string(),
+        age: z.number(),
+        breed: z.string()
+    })
+    .required();
+
+export type CreateCatDto = z.infer<typeof createCatSchema>;
