16c039c161f7bcc872d223341bac28a8047fe4df
Manases Lovera <manaseslovera07@gmail.com>
2024-10-13 14:13:23 -0400

    using class-validator

--- src/cats/dtos/cat.dto.ts
+++ src/cats/dtos/cat.dto.ts
@@ -1,16 +1,28 @@
+import { IsString, IsInt } from 'class-validator';
 
 export class CreateCatDto {
+    @IsString()
     public name: string;
+
+    @IsInt()
     public age: number;
+
+    @IsString()
     public breed: string;
 }
 
 export class UpdateCatDto {
+    @IsString()
     public name: string;
-    public age: string;
+
+    @IsInt()
+    public age: number;
+
+    @IsString()
     public breed: string;
 }
 
 export class ListAllEntities {
+    @IsInt()
     public limit: number;
 }
